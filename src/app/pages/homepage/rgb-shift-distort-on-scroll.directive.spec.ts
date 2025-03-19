936e1aa90373c7b5a9aac7766396098b185408df
sammunro94 <sam.munro@tepeo.com>
2025-03-19 15:53:10 +0000

    Auto-generated commit

--- src/app/pages/homepage/rgb-shift-distort-on-scroll.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RgbShiftDistortOnScrollDirective } from './rgb-shift-distort-on-scroll.directive';
-
-describe('RgbShiftDistortOnScrollDirective', () => {
-  it('should create an instance', () => {
-    const directive = new RgbShiftDistortOnScrollDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/pages/homepage/translate-to-canvas-coords.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TranslateToCanvasCoordsDirective } from './translate-to-canvas-coords.directive';
-
-describe('TranslateToCanvasCoordsDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TranslateToCanvasCoordsDirective();
-    expect(directive).toBeTruthy();
-  });
-});
